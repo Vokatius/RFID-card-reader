fb1b58c46a617eb114fcda02fc8296f6453ee0e4
= <=>
2026-03-11 22:11:31 +0100

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
+#include <vector>
 #include "ListMenu.h"
 #include "Input.h"
+#include "Base64.h"
 #include "Display_I2C.h"
 
 #define LED_PIN 2
@@ -11,44 +13,16 @@
 #define LCD_ROWS 2
 
 Display_I2C lcd(16,LCD_ROWS,0x27);
-ListMenu menu(LCD_ROWS, {"Scan", "Register", "Request", "Delete"});
-Input input({BTN_LEFT_PIN, BTN_MIDDLE_PIN, BTN_RIGHT_PIN});
-
-int calc_pointer(int cursor_pos);
 
 void setup() {
+    Serial.begin(9600);
     pinMode(LED_PIN, OUTPUT);
 
     lcd.Init();
+    const std::vector<uint8_t> test = { 0x02, 0xA1, 0x02, 0xA2, 0x12, 0xA1, 0x02 };
+    String res = Base64::to_base64(test);
+
+    lcd.Write(0,0, res);
 }
 
-void loop() {
-    auto items = menu.get_display_items();
-    for(int i = 0; i < LCD_ROWS; i++){
-        lcd.Write(2, i, items[i]);
-    }
-
-    lcd.Overwrite(0, calc_pointer(menu.get_cursor_pos()), "*");
-
-    int pressed_btn = input.get_button();
-
-    if(pressed_btn == BTN_LEFT_PIN)
-        menu.move_up();
-    else if(pressed_btn == BTN_MIDDLE_PIN) {
-        digitalWrite(LED_PIN, HIGH);
-        while (digitalRead(BTN_MIDDLE_PIN) == LOW)
-        { }        
-        digitalWrite(LED_PIN, LOW);
-    }
-    else if(pressed_btn == BTN_RIGHT_PIN) 
-        menu.move_down();
-
-    delay(50);
-}
-
-int calc_pointer(int cursor_pos) {
-    if(cursor_pos < LCD_ROWS)
-        return cursor_pos;
-
-    return cursor_pos - LCD_ROWS;
-}
+void loop() { }
