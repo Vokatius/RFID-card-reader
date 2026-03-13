ea5d9ccc4396e6b0b17001b551592b8209034f73
= <=>
2026-03-13 16:10:38 +0100

    Implemented D1602_Button_IO

--- include/io_implementations/1602_push_buttons/d1602_button_io.h
+++ include/io_implementations/1602_push_buttons/d1602_button_io.h
@@ -5,8 +5,14 @@
 #include "hardware\input\Push_Button_Input.h"
 #include "services\io_service_base.h"
 
-class D1602_Button_IO : IO_Service_Base<Push_Button_Input, Display_I2C> {
+class D1602_Button_IO : public IO_Service_Base {
+    private:
+        Display_I2C& m_display;
+        Push_Button_Input& m_buttons;
 
+    public:
+        D1602_Button_IO(IRfid_Reader& reader, Display_I2C& display, Auth_Service& auth, Push_Button_Input& buttons, uint8_t max_name_len);
+        void init();
 };
 
 #endif
--- src/io_implementations/1602_push_buttons/d1602_button_io.cpp
+++ src/io_implementations/1602_push_buttons/d1602_button_io.cpp
@@ -1,1 +1,14 @@
 #include "io_implementations\1602_push_buttons\d1602_button_io.h"
+
+D1602_Button_IO::D1602_Button_IO(
+    IRfid_Reader& reader, 
+    Display_I2C& display, 
+    Auth_Service& auth,
+    Push_Button_Input& buttons, 
+    uint8_t max_name_len
+) : IO_Service_Base(reader, auth, max_name_len), 
+    m_display(display),
+    m_buttons(buttons)
+{ }
+
+void D1602_Button_IO::init() { }
